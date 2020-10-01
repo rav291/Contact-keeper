31531b46b9f7bb9d96ba67e2f616b17dc6f83f51
Ravi Anand <ravianand291@gmail.com>
2020-10-01 15:19:27 +0530

    Final Commit and Deplyoment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path')
+const path = require('path');
 
 const app = express();
 
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
 
     app.get('*', (req, res) => { // * means everything except the routes above
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
